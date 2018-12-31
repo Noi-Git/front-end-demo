02a7ad4cf9813d63d14586ca87bf1151d8ae635e
Noi-Git <noi.patsin@gmail.com>
2018-12-31 02:14:52 -0800

    style the page

--- gulp/tasks/watch.js
+++ gulp/tasks/watch.js
@@ -2,7 +2,7 @@ const gulp  = require('gulp'),
 watch       = require('gulp-watch'),
 browserSync = require('browser-sync').create();
 
-gulp.task('watch', () => {
+gulp.task('watch', function() {
 
   browserSync.init({
     notify: false,
@@ -11,17 +11,17 @@ gulp.task('watch', () => {
     }
   })
 
-  watch('./app/index.html', () => {
+  watch('./app/index.html', function() {
     browserSync.reload()
   })
 
-  watch('./app/assets/styles/**/*.css', () => {
+  watch('./app/assets/styles/**/*.css', function() {
     gulp.start('cssInject')
   })
 
 })
 
-gulp.task('cssInject', ['styles'], () => {
+gulp.task('cssInject', ['styles'], function() {
   return gulp.src('./app/temp/styles/styles.css')
     .pipe(browserSync.stream())
 })
